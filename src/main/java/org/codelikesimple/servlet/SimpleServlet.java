f50b404ab48ff4c837438bab2e369001470ea057
vicente.garcia <footfix73@gmail.com>
2023-05-23 13:56:15 +0200

    Probando el push automatico con openshift

--- src/main/java/org/codelikesimple/servlet/SimpleServlet.java
+++ src/main/java/org/codelikesimple/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Hola que es lo k ase" + "</h1>");
+		out.println("<h1>" + "Hola que es lo k ase aqui" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
